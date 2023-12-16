bc7886f5c711a7f2015a9f7cc0421778a6742550
Suraj Jain <imsuraj.sj@gmail.com>
2023-12-16 11:35:51 +0530

    Initial Commit

---
+++ Clipboard Editor/Clipboard_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_EditorApp.swift
+//  Clipboard Editor
+//
+//  Created by Suraj Jain on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
